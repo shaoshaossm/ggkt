ea5383b0fc33528ec773c0f1f0f838a95038a27e
shaoshao <1600767556@qq.com>
2022-10-10 17:04:06 +0800

    TeacherMapper

---
+++ service/service_vod/src/main/java/com/ssm/ggkt/vod/mapper/TeacherMapper.java
@@ -0,0 +1,17 @@
+package com.ssm.ggkt.vod.mapper;
+
+import com.ssm.ggkt.model.vod.Teacher;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 讲师 Mapper 接口
+ * </p>
+ *
+ * @author ssm
+ * @since 2022-10-09
+ */
+public interface TeacherMapper extends BaseMapper<Teacher> {
+
+}
