4a8810c2de87eb23e40a4d11e883f5b4f78798d3
shaoshao <1600767556@qq.com>
2022-10-10 17:04:33 +0800

    TeacherService

---
+++ service/service_vod/src/main/java/com/ssm/ggkt/vod/service/impl/TeacherServiceImpl.java
@@ -0,0 +1,21 @@
+package com.ssm.ggkt.vod.service.impl;
+
+import com.ssm.ggkt.model.vod.Teacher;
+
+import com.ssm.ggkt.vod.mapper.TeacherMapper;
+import com.ssm.ggkt.vod.service.TeacherService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 讲师 服务实现类
+ * </p>
+ *
+ * @author ssm
+ * @since 2022-10-09
+ */
+@Service
+public class TeacherServiceImpl extends ServiceImpl<TeacherMapper, Teacher> implements TeacherService {
+
+}
