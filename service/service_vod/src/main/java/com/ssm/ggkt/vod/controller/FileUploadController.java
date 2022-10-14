b954723c9d122bee7b1dab7ae4ab7f5555374668
shaoshao <1600767556@qq.com>
2022-10-14 11:20:04 +0800

    跨域注解添加

--- service/service_vod/src/main/java/com/ssm/ggkt/vod/controller/FileUploadController.java
+++ service/service_vod/src/main/java/com/ssm/ggkt/vod/controller/FileUploadController.java
@@ -5,6 +5,7 @@ import com.ssm.ggkt.vod.service.FileService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Api(tags = "文件上传接口")
 @RestController
 @RequestMapping("/admin/vod/file")
+@CrossOrigin
 public class FileUploadController {
 
     @Autowired
