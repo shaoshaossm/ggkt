1a97e1bd4675835391aefe5b4655ee053ff0e372
shaoshao <1600767556@qq.com>
2022-10-16 14:10:50 +0800

    导入导出课程实现

--- service/service_vod/src/main/java/com/ssm/ggkt/vod/service/impl/SubjectServiceImpl.java
+++ service/service_vod/src/main/java/com/ssm/ggkt/vod/service/impl/SubjectServiceImpl.java
@@ -1,13 +1,26 @@
 package com.ssm.ggkt.vod.service.impl;
 
+import com.alibaba.excel.EasyExcel;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import com.ssm.ggkt.exception.GgktException;
 import com.ssm.ggkt.model.vod.Subject;
+import com.ssm.ggkt.vo.vod.SubjectEeVo;
+import com.ssm.ggkt.vo.vod.SubjectVo;
+import com.ssm.ggkt.vod.listener.SubjectListener;
 import com.ssm.ggkt.vod.mapper.SubjectMapper;
 import com.ssm.ggkt.vod.service.SubjectService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.sun.deploy.net.URLEncoder;
 import org.apache.velocity.runtime.directive.Foreach;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,11 +34,12 @@ import java.util.List;
 @Service
 public class SubjectServiceImpl extends ServiceImpl<SubjectMapper, Subject> implements SubjectService {
 
+    @Autowired
+    private SubjectListener subjectListener;
     /**
-     *
      * @param id 要查询的id
      * @return true or false
-     *
+     * <p>
      * 这段代码的意思是传入一个id，查询条件为id = parent_id，查询所有与parent_id有关的的数据后
      * 获取这些数据的id值，然后再次判断查询表中的数据，判断是否有数据的parent_id与这个id相同，
      * 相同的话返回的数据打小就大于0，返回true，即设置setHasChildren为true，
@@ -44,6 +58,41 @@ public class SubjectServiceImpl extends ServiceImpl<SubjectMapper, Subject> impl
         }
         return subjectList;
     }
+
+    @Override
+    public void exportData(HttpServletResponse response) {
+        try {
+            response.setContentType("application/vnd.ms-excel");
+            response.setCharacterEncoding("utf-8");
+            // 这里URLEncoder.encode可以防止中文乱码 当然和easyexcel没有关系
+            String fileName = URLEncoder.encode("课程分类", "UTF-8");
+            response.setHeader("Content-disposition", "attachment;filename=" + fileName + ".xlsx");
+            // 查询课程分类表所有数据
+            List<Subject> subjectList = baseMapper.selectList(null);
+
+            ArrayList<SubjectEeVo> subjectEeVos = new ArrayList<>();
+            for (Subject subject : subjectList) {
+                SubjectEeVo subjectEeVo = new SubjectEeVo();
+                BeanUtils.copyProperties(subject, subjectEeVo);
+                subjectEeVos.add(subjectEeVo);
+            }
+
+            EasyExcel.write(response.getOutputStream(), SubjectEeVo.class).sheet("课程分类").doWrite(subjectEeVos);
+
+        } catch (Exception e) {
+            throw new GgktException(20001,"导出失败");
+        }
+    }
+
+    @Override
+    public void importData(MultipartFile file) {
+        try {
+            EasyExcel.read(file.getInputStream(),SubjectEeVo.class,subjectListener).sheet().doRead();
+        } catch (IOException e) {
+            throw new GgktException(20001,"导入失败");
+        }
+    }
+
 
     private boolean isChildren(Long subjectId) {
         QueryWrapper<Subject> wrapper = new QueryWrapper<>();
