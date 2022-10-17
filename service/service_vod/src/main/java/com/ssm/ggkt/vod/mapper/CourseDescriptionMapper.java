e2756349eafd7610bfef1447f6bbae345cb82b6d
shaoshao <1600767556@qq.com>
2022-10-17 13:03:46 +0800

    CourseDescription

---
+++ service/service_vod/src/main/java/com/ssm/ggkt/vod/mapper/CourseDescriptionMapper.java
@@ -0,0 +1,16 @@
+package com.ssm.ggkt.vod.mapper;
+
+import com.ssm.ggkt.model.vod.CourseDescription;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 课程简介 Mapper 接口
+ * </p>
+ *
+ * @author ssm
+ * @since 2022-10-16
+ */
+public interface CourseDescriptionMapper extends BaseMapper<CourseDescription> {
+
+}
---
+++ service/service_vod/src/main/java/com/ssm/ggkt/vod/service/impl/CourseDescriptionServiceImpl.java
@@ -0,0 +1,20 @@
+package com.ssm.ggkt.vod.service.impl;
+
+import com.ssm.ggkt.model.vod.CourseDescription;
+import com.ssm.ggkt.vod.mapper.CourseDescriptionMapper;
+import com.ssm.ggkt.vod.service.CourseDescriptionService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 课程简介 服务实现类
+ * </p>
+ *
+ * @author ssm
+ * @since 2022-10-16
+ */
+@Service
+public class CourseDescriptionServiceImpl extends ServiceImpl<CourseDescriptionMapper, CourseDescription> implements CourseDescriptionService {
+
+}
