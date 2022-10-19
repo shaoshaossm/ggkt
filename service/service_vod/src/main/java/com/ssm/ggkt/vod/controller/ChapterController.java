5f508fb1c0ddc28f30dfe977c99a7050376774ba
shaoshao <1600767556@qq.com>
2022-10-19 16:03:44 +0800

    章节的增删改查

---
+++ service/service_vod/src/main/java/com/ssm/ggkt/vod/controller/ChapterController.java
@@ -0,0 +1,67 @@
+package com.ssm.ggkt.vod.controller;
+
+
+import com.ssm.ggkt.model.vod.Chapter;
+import com.ssm.ggkt.result.Result;
+import com.ssm.ggkt.vo.vod.ChapterVo;
+import com.ssm.ggkt.vod.service.ChapterService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 课程 前端控制器
+ * </p>
+ *
+ * @author ssm
+ * @since 2022-10-18
+ */
+@Api(tags = "章节管理接口")
+@RestController
+@RequestMapping("/admin/vod/chapter")
+@CrossOrigin
+public class ChapterController {
+
+    @Autowired
+    private ChapterService chapterService;
+
+    @ApiOperation("课程大纲")
+    @GetMapping("getNestedTreeList/{courseId}")
+    public Result getTreeList(@PathVariable Long courseId) {
+        List<ChapterVo> list = chapterService.getTreeList(courseId);
+        return Result.ok(list);
+    }
+
+    @ApiOperation("添加章节")
+    @PostMapping("save")
+    public Result save(@RequestBody Chapter chapter) {
+        chapterService.save(chapter);
+        return Result.ok(null);
+    }
+
+    @ApiOperation("修改-根据id查询")
+    @GetMapping("get/{id}")
+    public Result get(@PathVariable Long id) {
+        Chapter chapter = chapterService.getById(id);
+        return Result.ok(chapter);
+    }
+
+    @ApiOperation("修改章节最终实现")
+    @PostMapping("update")
+    public Result update(@RequestBody Chapter chapter) {
+        chapterService.updateById(chapter);
+        return Result.ok(null);
+    }
+
+    @ApiOperation("删除章节")
+    @DeleteMapping("remove/{id}")
+    public Result remove(@PathVariable Long id) {
+        chapterService.removeById(id);
+        return Result.ok(null);
+    }
+}
+
