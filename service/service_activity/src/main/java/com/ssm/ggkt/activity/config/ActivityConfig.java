296a27bc3509cd27d221892b0666ba292d129009
shaoshao <1600767556@qq.com>
2022-10-26 12:46:34 +0800

    分页插件

---
+++ service/service_activity/src/main/java/com/ssm/ggkt/activity/config/ActivityConfig.java
@@ -0,0 +1,23 @@
+package com.ssm.ggkt.activity.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2022/10/24 21:13
+ */
+@Configuration
+@MapperScan("com.ssm.ggkt.activity.mapper")
+public class ActivityConfig {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
